dd61d7492e85c0e177738cbedd06ab4cf28ec0aa
liutao <liut@huitone.com>
2023-04-26 14:23:16 +0800

    build sh

--- build.sh
+++ build.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 # Build Windows x86 binary
+echo 'Build Windows x86 binary'
 GOOS=windows GOARCH=386 go build -o FileSlice_windows_386.exe main.go
 
 # Build Windows amd64 binary
+echo 'Build Windows amd64 binary'
 GOOS=windows GOARCH=amd64 go build -o FileSlice_windows_amd64.exe main.go
 
 # Build Mac OS X x86 binary
+echo 'Build Mac OS X x86 binary'
 GOOS=darwin GOARCH=386 go build -o FileSlice_darwin_386 main.go
 
 # Build Mac OS X amd64 binary
+echo 'Build Mac OS X amd64 binary'
 GOOS=darwin GOARCH=amd64 go build -o FileSlice_darwin_amd64 main.go
 
 # Build Linux x86 binary
+echo 'Build Linux x86 binary'
 GOOS=linux GOARCH=386 go build -o FileSlice_linux_386 main.go
 
 # Build Linux amd64 binary
+echo 'Build Linux amd64 binary'
 GOOS=linux GOARCH=amd64 go build -o FileSlice_linux_amd64 main.go
 
 # Build Linux ARM64 binary
+echo 'Build Linux ARM64 binary'
 GOOS=linux GOARCH=arm64 go build -o FileSlice_linux_arm64 main.go
 
 # Build Mac OS X ARM64 binary
+echo 'Build Mac OS X ARM64 binary'
 GOOS=darwin GOARCH=arm64 go build -o FileSlice_darwin_arm64 main.go
