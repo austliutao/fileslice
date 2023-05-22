4a20f25f97255331f661e59be89bba2437da53fa
liutao <liut@huitone.com>
2023-05-22 11:23:05 +0800

    fix 2.1

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 )
+
+type FileData struct {
+	MD5    string `json:"MD5"`
+	SHA1   string `json:"SHA1"`
+	SHA256 string `json:"SHA256"`
+}
 
 func main() {
 	var isSplit bool
@@ -21,14 +31,15 @@ func main() {
 		return
 	}
 
+	var err error
 	if isSplit {
-		err := splitFile(filename, chunkSize)
+		err = splitFile(filename, chunkSize)
 		if err != nil {
 			fmt.Printf("Error splitting file: %v\n", err)
 			return
 		}
 	} else {
-		err := recoverFile(filename)
+		err = recoverFile(filename)
 		if err != nil {
 			fmt.Printf("Error recovering file: %v\n", err)
 			return
@@ -43,15 +54,49 @@ func splitFile(filename string, chunkSize int) error {
 	}
 	defer file.Close()
 
+	// Get file information
 	fileInfo, err := file.Stat()
 	if err != nil {
 		return err
 	}
 
+	// Calculate total number of chunks
 	chunkSizeBytes := int64(chunkSize * 1024 * 1024)
 	totalChunks := int(fileInfo.Size()/chunkSizeBytes) + 1
 
+	// Create JSON file to hold file data
+	jsonFile, err := os.Create(filename + ".json")
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	// Calculate file hashes
+	md5Hash, sha1Hash, sha256Hash, err := getFileHashes(file)
+	if err != nil {
+		return err
+	}
+
+	// Write file data to JSON file
+	fileData := FileData{MD5: md5Hash, SHA1: sha1Hash, SHA256: sha256Hash}
+	fileDataJSON, err := json.Marshal(fileData)
+	if err != nil {
+		return err
+	}
+	_, err = jsonFile.Write(fileDataJSON)
+	if err != nil {
+		return err
+	}
+
+	// Reset file pointer to start of file
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	// Split file into chunks
 	for i := 0; i < totalChunks; i++ {
+		// Create chunk file
 		chunkFileName := fmt.Sprintf("%s.%d", filename, i)
 		chunkFile, err := os.Create(chunkFileName)
 		if err != nil {
@@ -59,6 +104,7 @@ func splitFile(filename string, chunkSize int) error {
 		}
 		defer chunkFile.Close()
 
+		// Write chunk data to file
 		buf := make([]byte, 4*1024) // 4KB buffer
 		var bytesWritten int64
 
@@ -84,18 +130,47 @@ func splitFile(filename string, chunkSize int) error {
 }
 
 func recoverFile(filename string) error {
+	// Open JSON file to get file data
+	jsonFile, err := os.Open(filename + ".json")
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	var fileData FileData
+	err = json.NewDecoder(jsonFile).Decode(&fileData)
+	if err != nil {
+		return err
+	}
+
+	// Create new file for recovered data
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
+	// Create hash functions
+	md5Hash := md5.New()
+	sha1Hash := sha1.New()
+	sha256Hash := sha256.New()
+
+	// Process each chunk of the file
 	i := 0
 	for {
+		// Open next chunk file
 		chunkFileName := fmt.Sprintf("%s.%d", filename, i)
 		_, err := os.Stat(chunkFileName)
 		if os.IsNotExist(err) {
-			// No more chunks
+			// No more chunks, verify hash values
+			md5Value := fmt.Sprintf("%x", md5Hash.Sum(nil))
+			sha1Value := fmt.Sprintf("%x", sha1Hash.Sum(nil))
+			sha256Value := fmt.Sprintf("%x", sha256Hash.Sum(nil))
+
+			if md5Value != fileData.MD5 || sha1Value != fileData.SHA1 || sha256Value != fileData.SHA256 {
+				return fmt.Errorf("hash values do not match")
+			}
+
 			break
 		}
 
@@ -105,13 +180,30 @@ func recoverFile(filename string) error {
 		}
 		defer chunkFile.Close()
 
-		_, err = io.Copy(file, chunkFile)
-		if err != nil {
-			return err
-		}
+		// Update hash values and write chunk data to file
+		io.Copy(io.MultiWriter(file, md5Hash, sha1Hash, sha256Hash), chunkFile)
 
 		i++
 	}
 
+	// Print verification result to console
+	fmt.Println("File verification successful!")
+
 	return nil
+}
+
+func getFileHashes(file *os.File) (string, string, string, error) {
+	// Create hash functions
+	md5Hash := md5.New()
+	sha1Hash := sha1.New()
+	sha256Hash := sha256.New()
+
+	// Process file data
+	_, err := io.Copy(io.MultiWriter(md5Hash, sha1Hash, sha256Hash), file)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	// Return hash values as strings
+	return fmt.Sprintf("%x", md5Hash.Sum(nil)), fmt.Sprintf("%x", sha1Hash.Sum(nil)), fmt.Sprintf("%x", sha256Hash.Sum(nil)), nil
 }
